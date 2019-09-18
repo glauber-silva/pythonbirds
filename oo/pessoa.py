1e1455c8df23923c0c7135395cdcd4ac643fc3b4
Glauber Silva <glauber.silva@lendico.com.br>
2019-09-18 10:45:01 -0300

    add attributes nome and idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,19 @@
 class Pessoa:
+    def __init__(self, nome: str = None, idade: int = 35):
+        self.idade = idade
+        self.nome = nome
+
 
     def cumprimentar(self):
         return f"Olá {id(self)}"
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Lucio')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = "Glauber"
+    print(p.nome)
+    print(p.idade)
